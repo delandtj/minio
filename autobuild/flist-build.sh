0f79a4763b65b602a75bbfe466323be6aa02cb25
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2018-03-16 10:37:36 +0200

    improve autobuild script
    - make binary executable
    - add missing required folder

--- autobuild/flist-build.sh
+++ autobuild/flist-build.sh
@@ -5,8 +5,11 @@ apt-get update
 apt-get install wget build-essential -y
 
 # make output directory
-mkdir -p /tmp/archives/
-mkdir -p /tmp/bin
+ARCHIVE=/tmp/archives
+FLIST=/tmp/flist
+mkdir -p $ARCHIVE
+mkdir -p $FLIST/bin/zerostor_meta
+mkdir -p $FLIST/tmp
 
 # install go
 wget https://dl.google.com/go/go1.10.linux-amd64.tar.gz
@@ -20,16 +23,18 @@ mkdir -p  /root/go/src/github.com/minio
 mv /minio /root/go/src/github.com/minio/minio
 pushd /root/go/src/github.com/minio/minio
 make build
-mv minio /tmp/bin
+mv minio $FLIST/bin
 popd
 
 # install restic
 pushd /tmp
 wget https://github.com/restic/restic/releases/download/v0.8.3/restic_0.8.3_linux_amd64.bz2
 bzip2 -d restic_0.8.3_linux_amd64.bz2
-mv restic_0.8.3_linux_amd64 /tmp/bin/restic
+mv restic_0.8.3_linux_amd64 $FLIST/bin/restic
 popd
 
+# make sure binary is executable
+chmod +x $FLIST/bin/*
 
 
-tar -czf "/tmp/archives/minio.tar.gz" -C /tmp/ bin
+tar -czf "/tmp/archives/minio.tar.gz" -C $FLIST .
